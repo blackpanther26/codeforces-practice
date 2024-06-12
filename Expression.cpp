701cfac3d4ce0ac72a4d4dfd938953d290e7418f
blackpanther26 <priyanshuchahal1@gmail.com>
2024-06-12 16:57:45 +0530

    new commit

--- Expression.cpp
+++ Expression.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int t ;
-    cin>>t;
-    while (t--)
-    {
-        
-    }
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    int ans = a + b + c;
+    ans = max(ans, a * b * c);
+    ans = max(ans, (a + b) * c);
+    ans = max(ans, a * (b + c));
+    cout << ans;
 }
